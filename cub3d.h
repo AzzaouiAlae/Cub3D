9bff97cac68c264f1b8480e0e252143dba44ec75
AzzaouiAlae <azzaoui@outlook.com>
2025-08-11 19:50:44 +0100

    change player speed

--- cub3d.h
+++ cub3d.h
@@ -18,10 +18,10 @@
 # define M_M_MARGIN_Y 40.0
 # define M_M_BORDER_SIZE 1.0
 # define M_M_TIAL_SIZE 40
-# define PLAYER_SPEED 1.0
+# define PLAYER_SPEED 2.0
 # define FRAME_PER_SEC 60
 # define M_PI 3.14159265358979323846
-# define ANGLE_SPEED 1.0
+# define ANGLE_SPEED 2.0
 # define FOV 60.0
 
 typedef enum e_side
